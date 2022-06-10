f173a7dfd4e8a32f556a0a305f1fc9c96d44c5e9
ADM-SISTEMASP01 <ADM-SISTEMASP01@eseimsalud.local>
2022-06-10 14:57:37 -0500

    validar login ok

--- Creaciones Asbleidy/servidor/valida_login.php
+++ Creaciones Asbleidy/servidor/valida_login.php
@@ -1,19 +1,47 @@
 <?php
 
- include 'conexion.php'
+ include 'conexion.php';
 
- $todos = '*';
- $consulta = "SELECT $todos from usuario";
+ $email = $_POST['email'];
+ $pass = $_POST['pass'];
+
+
+ $ux = '';
+ $t = '*';
+ $consulta = "SELECT $t from usuario";
 
  $resultado = mysqli_query($mysqli, $consulta);
 
  if ($resultado) {
     while ($row = $resultado->fetch_array()) {
-      $email = $row['email'];
-      $pass = $row['pass'];
+      $email_v = $row['email'];
+      $pass_v = $row['pass'];
+
+     // echo $email_v;
+     // echo $pass_v;
+
+      if ($email===$email_v and $pass==$pass_v){
+        //header("Location:../cliente/vistas/login.html");
+        header("Location:../cliente/vistas/tienda.html");
+
+        //valida en cascada OJO
 
 
-      echo $email;
-      echo $pass;
+      }else{
+       // header("Location:../cliente/vistas/tienda.html");
+      echo '<script type ="text/JavaScript">';  
+      echo 'alert("ACCESO DENEGADO")';  
+      echo '</script>';  
+
+      //PENDIENTE CONTENTENER LA ALTERTA Y LUEGO LLEVARLA A LA VISTA
+
+       header("Location:../cliente/vistas/login.html");
+      }
     }
- }
+
+    
+  }
+ 
+
+
+//aqui ya muestra la consulta
